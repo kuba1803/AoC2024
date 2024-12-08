97e0c7be3e48497e05995376e71542ef453e5bce
Jakub <kuba1803@ymail.com>
2024-12-08 22:01:15 +0100

    day 6

--- day_6/part_2.py
+++ day_6/part_2.py
@@ -5,6 +5,24 @@ oTab = []
 posX, posY = 0, 0
 sum = 0
 
+def printRead(map,mask,oTab):
+    for y in range(len(oTab)):
+        tab = []
+        for x in range(len(oTab[y])):
+            if (map[y][x] == "#"):
+                tab.append("#")
+            elif (oTab[y][x] == 1):
+                tab.append("O")
+            elif ((mask[y][x] & 1 == 1 or mask[y][x] & 4 == 4) and (mask[y][x] & 2 == 2 or mask[y][x] & 8 == 8)):
+                tab.append("+")
+            elif (mask[y][x] & 1 == 1 or mask[y][x] & 4 == 4):
+                tab.append("|")
+            elif (mask[y][x] & 2 == 2 or mask[y][x] & 8 == 8):
+                tab.append("-")
+            else:
+                tab.append(".")
+        print(tab)
+
 def nextPosition( posY, posX, dir):
     if dir == '^':
         return posY - 1, posX
@@ -41,9 +59,11 @@ def checkRepeatStep(mask,posY, posX, dir):
         return False
     return True
 
-def appliedMove(map, mask,posY, posX, dir):
+def appliedMove(map, mask,posY, posX,nextY, nextX, dir, nextDir):
     mask[posY][posX] |= getDirMask(dir)
-    map[posY][posX] = dir
+    map[nextY][nextX] = nextDir
+    if (nextY != posY or nextX != posX):
+        map[posY][posX] = "."
 
 def checkLoop(map, mask, curPosY, curPosX, dir, otab):
     loopPosY, loopPosX = nextPosition(curPosY, curPosX, dir)
@@ -68,35 +88,30 @@ def checkLoop(map, mask, curPosY, curPosX, dir, otab):
 
     while True:
         nextDir = loopMap[posY][posX]
+
+        if (checkRepeatStep(loopMask, posY, posX, nextDir)):
+            otab[loopPosY][loopPosX] = 1
+            return True
+
         nextY, nextX = nextPosition( posY, posX, nextDir)
         if( nextY < 0 or nextY >= len(loopMap) or nextX < 0 or nextX >= len(loopMap[nextY])):
             break
 
         if( loopMap[nextY][nextX]=="#"):
             nextDir = changeDirection(nextDir)
-            nextY, nextX = posY, nextX
+            nextY, nextX = posY, posX
 
         if( nextY < 0 or nextY >= len(loopMap) or nextX < 0 or nextX >= len(loopMap[nextY])):
             break
 
-        if(checkRepeatStep(loopMask,nextY,nextX,nextDir)):
-            print("curPosX", curPosX, "curPosY", curPosY, "dir", dir)
-            print("loopPosX", loopPosX, "loopPosY", loopPosY)
-            for i in range(len(loopMap)):
-                print(loopMap[i])
-            otab[loopPosY][loopPosX] = 1
-            return True
-
-        appliedMove(loopMap, loopMask, nextY, nextX, nextDir)
-        if (nextY != posY or nextX != posX):
-            loopMap[posY][posX] = "."
+        appliedMove(loopMap, loopMask,posY,posX, nextY, nextX,loopMap[posY][posX], nextDir)
         posX = nextX
         posY = nextY
 
     return False
 
 
-with open('input_example.txt') as f:
+with open('input_file.txt') as f:
     for line in f:
         map.append(list(line.strip()))
         mask.append([0]*len(map[0]))
@@ -122,9 +137,7 @@ with open('input_example.txt') as f:
         if( nextY < 0 or nextY >= len(map) or nextX < 0 or nextX >= len(map[nextY])):
             break
 
-        appliedMove(map, mask, nextY, nextX, nextDir)
-        if(nextY!=posY or nextX!=posX):
-            map[posY][posX] = "."
+        appliedMove(map, mask,posY,posX, nextY, nextX,map[posY][posX], nextDir)
         posX = nextX
         posY = nextY
 
@@ -133,28 +146,6 @@ for y in range(len(map)):
         sum+=oTab[y][x]
 
 print(sum)
-for y in range(len(map)):
-    print(map[y])
 
-for y in range(len(mask)):
-    print(mask[y])
 
-for y in range(len(oTab)):
-    tab = []
-    for x in range(len(oTab[y])):
-        if( map[y][x]=="#"):
-            tab.append("#")
-        elif(oTab[y][x]==1):
-            tab.append("O")
-        elif((mask[y][x]&1==1 or mask[y][x]&4==4)and(mask[y][x]&2==2 or mask[y][x]&8==8) ):
-            tab.append("+")
-        elif(mask[y][x]&1==1 or mask[y][x]&4==4):
-            tab.append("|")
-        elif(mask[y][x]&2==2 or mask[y][x]&8==8):
-            tab.append("-")
-        else:
-            tab.append(".")
-    print(tab)
-
-print(mask[1][4]&1)
-print((mask[1][4]&2==1 or mask[1][4]&8==1))
+printRead(map,mask,oTab)
